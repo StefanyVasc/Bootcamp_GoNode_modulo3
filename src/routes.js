eb3b77b469e86aef0eebfa8390e6ce220b4dafd1
Stefany Sá <stefany.vasc.sa@gmail.com>
2019-03-22 16:37:48 -0300

    middleware de autenticação

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const routes = express.Router()
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const authMiddleware = require('./app/middlewares/auth')
+
+const controllers = require('./app/controllers')
 
 // rota para a criação do usuário
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
+
+routes.get('/teste', authMiddleware, (req, res) => {
+  res.json({ ok: true })
+})
 
 module.exports = routes
